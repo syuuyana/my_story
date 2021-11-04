f126b3f9240d2b8d15e842b8b37bee29a734255d
syuuyana <yananojimu@gmail.com>
2021-11-04 18:45:36 +0900

    comment initialize.sql

--- mysql/sqls/initialize.sql
+++ mysql/sqls/initialize.sql
@@ -1,7 +1,9 @@
+-- database app 作成 --
 CREATE DATABASE IF NOT EXISTS app;
 USE app;
 
 
+-- table Profile 作成 --
 CREATE TABLE IF NOT EXISTS Profiles(
   id int PRIMARY KEY AUTO_INCREMENT,
   email varchar(50) NOT NULL,
@@ -9,10 +11,9 @@ CREATE TABLE IF NOT EXISTS Profiles(
   tel varchar(20) NOT NULL,
   name varchar(20) NOT NULL,
   times int DEFAULT 0
---  comment varchar(500) DEFAULT "Hello World!",
---  icon varchar(100) DEFAULT "static/default_user_icon.png"
 );
 
 
+-- テストデータ挿入 --
 INSERT INTO Profiles (email, password, tel, name) VALUES("syuu@email.com", "Password1", "09012345678", "syuu");
 INSERT INTO Profiles (email, password, tel, name) VALUES("yana@email.com", "Password2", "08012345678", "yana");
